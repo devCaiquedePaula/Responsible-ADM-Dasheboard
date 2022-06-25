0ffd5b555f6b646cdc1f50f27dec13c6d03f58f8
Caique de Paula Nascimento <104093663+devCaiquedePaula@users.noreply.github.com>
2022-06-25 10:59:11 -0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'LARVENDER KF102 Drone',
+    productNumber: '32119',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'Keychron k4v2 Wireless',
+    productNumber: '38576',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Drone with Camera Drone',
+    productNumber: '95736',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'GPS 4K Drone',
+    productNumber: '67484',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'HeadSet G733',
+    productNumber: '15164',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+]
